53e70560d9effdcaf8358c580998a759600bbd59
= <=>
2018-07-16 21:42:45 -0500

    updated index js and css Jul16

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,7 @@
 $("#submit").on("click", function(event){
     event.preventDefault();
     $("#article-section").empty();
-    var birthday = $("#bday").val();
+    var birthday = $("#date-input").val();
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
       'api-key': "c2a6882d365346fabaad70c46f8eb4bf",
@@ -29,11 +29,18 @@ $("#submit").on("click", function(event){
                   headline +
                   "</strong>");
             if (byline && byline.original) {
-            $articleListItem.append("<h5>" + byline.original + "</h5>");};
+            $articleListItem.append("<h5>" + byline.original + "</h5>");}
             $articleListItem.append("<h6>" + article.snippet + "</h6>");
             $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
             $articleList.append($articleListItem);
-              }})});
+        }});
+          });
+
+
+
+
+
+
               var modal = document.getElementById('myModal');
 
               // Get the button that opens the modal
@@ -45,16 +52,16 @@ $("#submit").on("click", function(event){
               // When the user clicks on the button, open the modal
               btn.onclick = function() {
                   modal.style.display = "block";
-              }
+              };
 
               // When the user clicks on <span> (x), close the modal
               span.onclick = function() {
                   modal.style.display = "none";
-              }
+              };
 
               // When the user clicks anywhere outside of the modal, close it
               window.onclick = function(event) {
                   if (event.target == modal) {
                       modal.style.display = "none";
                   }
-              }
+              };
